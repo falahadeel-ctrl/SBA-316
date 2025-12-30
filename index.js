c0f0958b62c7a64c229ed381e315bac4bd44470e
falahadeel-ctrl <falahadeel@gmail.com>
2025-12-30 14:26:49 -0500

    yjy

--- index.js
+++ index.js
@@ -12,7 +12,26 @@ for(let i=0; i<a.length; i++){
     new3[i].textcontent=a[i];
     new2.prepend(new3[i]);
 }
-
 new2.setAttribute("type","A");
-let grab=document.getElementbyName(fieldset);
+let score=0;
+
+
+//working on making a,b,c,d
+new2.addEventListener("click",function (event){
+    event.preventdefault;
+    if(ol.event.target==="D"){
+        score++;
+    }else{
+        return;
+    }
+})
+
+//total scoreing
+if(score==3){
+windows.alert("You Won!!!");}
+else{
+windows.alert("you were close try again");
+}
+
+let grab=document.getElementById("qwe");
 grab.prepend(fragment);
