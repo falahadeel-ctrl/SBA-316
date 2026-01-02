ffbdd2cbceb56f4c70ffcffff035e9ce21c684b5
falahadeel-ctrl <falahadeel@gmail.com>
2026-01-02 01:34:14 -0500

    fervf

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ newlegend.textContent="Fusce ut ex luctus, tempor justo non, maximus diam?";
 
 //creating options
 let a=["Pellentesque at ante at elit","scelerisque elementum","Aenean augue lorem,","acilisis ac nibh id"]
+let options = ['a','b','c','d']
 for(let i=0; i<a.length; i++){
 
     let list =document.createElement('li');
     let input =document.createElement("input");
     input.setAttribute("type","radio");
     input.setAttribute("name","form2");
-    input.setAttribute("value","a");
+    // for(z=0;z<a.length;z++){
+        input.setAttribute(`value`,options[i]);
+    // }
     list.textContent =a[i];
     neww.prepend(list);
     list.prepend(input);
@@ -29,41 +32,58 @@ container.textContent="Answer both questions and click Submit";
 fragment.appendChild(newlegend);
 fragment.appendChild(neww);
 //working on making a,b,c,d
-neww.addEventListener("click",function (event){
-    event.preventDefault();
-    if(event.target.value==="d"){
-        score++;
-    }else{
-        return;
-    }
+// neww.addEventListener("click",function (event){
+//     event.preventDefault();
+//     if(event.target.value==="d" || input.event.target.value==="a"){
+//         score++;
+//     }else{
+//         return;
+//     }
 
-});
+// });
 
 //second event listener(Change color,Show feedback,Use parent/child navigation,Disable radio buttons,Check answer)
-neww.addEventListener("change", function (event){
-event.preventDefault();
-let li = event.target.parentNode;
-if(event.target.value === "d"){
-  li.style.color='green';
-}else{
-  li.style.color='red';
-}
+
+// neww.addEventListener("change", function (event){
+// event.preventDefault();
+// let li = event.target.parentNode;
+// if(event.target.value === "d"){
+//   li.style.color='green';
+// }else{
+//   li.style.color='red';
+// }
+
 // if(event.target.tagname.textContent='submit'){
 //     let buttons = document.querySelectorAll('input');
 //     buttons.disabled=true;
 // }
-}
+// })
 
 form.addEventListener('click',function (event){
     event.preventDefault();
     let score=0;
-  if(event.target.id='submitbutton' && score===0)
+
+    let q1= document.querySelector(`input[name="form1"]:checked`);
+    let q2= document.querySelector(`input[name="form2"]:checked`);
+
+  if( !q1 || !q2){
+    window.alert('answer both questions!');
+    return;
+  }
+  if(q1.value==='a'){
+    score++;
+  }
+  if(q2.value==='d'){
+    score++;
+  }
+
+  if(event.target.id==='submitbutton' && score===0)
         {
         container.textContent="You got 0/2 correct. Try again!";
         container.style.color='red';
         window.alert('try again!');
     }
-    else if(event.target.id='submitbutton' && score===1)
+    else if(event.target.id=== 'submitbutton' && score===1)
         {
         container.textContent="You got 1/2 correct. Try again!";
         container.style.color='red';
@@ -76,12 +96,12 @@ form.addEventListener('click',function (event){
     }
 
     let radio = document.querySelectorAll('input[type="radio"]');
-    for(i=0;i<radio.length;i++){
-      radio[i].disabled='true';
+    for(let i=0;i<radio.length;i++){
+      radio[i].disabled=true;
     }
 
     let submit = document.getElementById('submitbutton');
-    submit.disabled='true';
+    submit.disabled=true;
 })
 
 //total scoreing
